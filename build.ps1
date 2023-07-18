84fad61b59862012515177dfbfdf9025dda9fd4e
Scott <scottandrewbonner@gmail.com>
2023-07-18 12:07:34 -0400

    change build script to delete old build

--- build.ps1
+++ build.ps1
@@ -1,3 +1,4 @@
+Remove-Item -Path ".\out" -Recurse
 New-Item -ItemType Directory -Name ".\out\InfiniteFusion" -Path ".\" -ErrorAction SilentlyContinue
 Copy-Item -Path ".\INSTALL_OR_UPDATE.bat" -Destination ".\out\InfiniteFusion" -Force
 Copy-Item -Path ".\REQUIRED_BY_INSTALLER_UPDATER" -Destination ".\out\InfiniteFusion" -Force -Recurse
