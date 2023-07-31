431443e49efb33d0a45dbde55d8682f6e5d85c7c
Scott <scottandrewbonner@gmail.com>
2023-07-31 09:35:33 -0400

    commented out read me

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 Remove-Item -Path ".\out" -Recurse
 New-Item -ItemType Directory -Name ".\out\InfiniteFusion" -Path ".\" -ErrorAction SilentlyContinue
 Copy-Item -Path ".\INSTALL_OR_UPDATE.sh" -Destination ".\out\InfiniteFusion" -Force
-Copy-Item -Path ".\ReadMe_InstallerUpdater.txt" -Destination ".\out\InfiniteFusion" -Force
+#Copy-Item -Path ".\ReadMe_InstallerUpdater.txt" -Destination ".\out\InfiniteFusion" -Force
 
 Compress-Archive -Path ".\out\InfiniteFusion" -DestinationPath ".\out\InfiniteFusion_linux.zip" -Force
 explorer .\out
