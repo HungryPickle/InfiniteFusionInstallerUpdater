0246b46e68046f9e954f05ecac63daf1275e26f7
Scott <scottandrewbonner@gmail.com>
2023-12-17 01:58:12 -0500

    kuray installer changes and gitconfig bugfixes

--- INSTALL_OR_UPDATE.bat
+++ INSTALL_OR_UPDATE.bat
@@ -1,17 +1,23 @@
 @echo off
 
-.\REQUIRED_BY_INSTALLER_UPDATER\7z.exe e -spf -aoa -o"REQUIRED_BY_INSTALLER_UPDATER" "REQUIRED_BY_INSTALLER_UPDATER\MinGit.7z"
+.\REQUIRED_BY_INSTALLER_UPDATER\7z.exe e -spf -aoa "REQUIRED_BY_INSTALLER_UPDATER\MinGit.7z"
 
 set mgit=".\REQUIRED_BY_INSTALLER_UPDATER\cmd\git.exe"
 %mgit% init .
-%mgit% remote add origin "https://github.com/infinitefusion/infinitefusion-e18.git"
-%mgit% fetch origin releases
-%mgit% reset --hard origin/releases
+%mgit% remote add origin "https://github.com/kurayamiblackheart/kurayshinyrevamp.git"
+%mgit% fetch origin release
+%mgit% reset --hard origin/release
 
-.\REQUIRED_BY_INSTALLER_UPDATER\7z.exe e -spf -aoa -o"Data" "Data\species.zip"
+@REM .\REQUIRED_BY_INSTALLER_UPDATER\7z.exe e -spf -aoa -o"Data" "Data\species.zip"
 
 echo:
 echo Installer/Updater made by Hungry Pickle
 echo:
 echo Thank you for installing or updating! You can close this window now.
-pause
+
+set arg1=%1
+if "%arg1%" == "auto" (
+    start "" .\Game.exe
+) else (
+    pause
+)
--- build.ps1
+++ build.ps1
@@ -1,11 +1,15 @@
 Remove-Item -Path ".\out" -Recurse
-New-Item -ItemType Directory -Name ".\out\InfiniteFusion" -Path ".\" -ErrorAction SilentlyContinue
-Copy-Item -Path ".\INSTALL_OR_UPDATE.bat" -Destination ".\out\InfiniteFusion" -Force
-Copy-Item -Path ".\REQUIRED_BY_INSTALLER_UPDATER" -Destination ".\out\InfiniteFusion" -Force -Recurse
-#Copy-Item -Path ".\ReadMe_InstallerUpdater.txt" -Destination ".\out\InfiniteFusion" -Force
+New-Item -ItemType Directory -Name ".\out" -Path ".\" -ErrorAction SilentlyContinue
+New-Item -ItemType Directory -Name ".\out\REQUIRED_BY_INSTALLER_UPDATER" -Path ".\" -ErrorAction SilentlyContinue
 
-Compress-Archive -Path ".\out\InfiniteFusion" -DestinationPath ".\out\InfiniteFusion.zip" -Force
-explorer .\out
+Start-Process "C:\Program Files\7-zip\7z.exe" -ArgumentList "a -spf -aoa `".\out\REQUIRED_BY_INSTALLER_UPDATER\MinGit.7z`" `".\REQUIRED_BY_INSTALLER_UPDATER`"" -Wait
+Copy-Item -Path ".\7z\*" -Destination ".\out\REQUIRED_BY_INSTALLER_UPDATER" -Force
+Copy-Item -Path ".\INSTALL_OR_UPDATE.bat" -Destination ".\out" -Force
+#Copy-Item -Path ".\REQUIRED_BY_INSTALLER_UPDATER" -Destination ".\out" -Force -Recurse
+#Copy-Item -Path ".\ReadMe_InstallerUpdater.txt" -Destination ".\out" -Force
+
+Compress-Archive -Path ".\out\*" -DestinationPath ".\out\KIF_Installer_by_HungryPickle.zip" -Force
+#explorer .\out
 
 Write-Host "Built:"(Get-Date -DisplayHint Time) 
 
