e6c595c6dd5e5638f06014a2fe9acaa18915ac93
Scott <scottandrewbonner@gmail.com>
2023-12-17 20:03:46 -0500

    ReadMe added

--- build.ps1
+++ build.ps1
@@ -1,7 +1,7 @@
 Remove-Item -Path ".\out" -Recurse
 New-Item -ItemType Directory -Name ".\out" -Path ".\" -ErrorAction SilentlyContinue
 Copy-Item -Path ".\INSTALL_OR_UPDATE.sh" -Destination ".\out" -Force
-#Copy-Item -Path ".\ReadMe_InstallerUpdater.txt" -Destination ".\out\InfiniteFusion" -Force
+Copy-Item -Path ".\ReadMe_InstallerUpdater.txt" -Destination ".\out" -Force
 
 Compress-Archive -Path ".\out\*" -DestinationPath ".\out\KIF_Linux_Installer_by_HungryPickle.zip" -Force
 #explorer .\out
